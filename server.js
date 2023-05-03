d584ef7b0e1acb229987f025e4dc6e6807172240
AhmedGhoniem01 <ahmed.ghoniem01@gmail.com>
2023-05-03 12:30:39 +0200

    initial setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const SpeakerService = require('./services/service')
+const FeedbackService = require('./services/feedback')
+const routes = require('./routes')
+
+//Handle views engine if used
+// app.set('view-engine', 'ejs')
+// app.set('views', path.join(__dirname, 'views'))
+
+//Handle static files if used
+// app.use(express.static(path.join(__dirname, 'static')))
+
+//Handling request body and json data
+app.use(express.urlencoded({extended: true})) 
+app.use(express.json())
+
+//Creating new services instances to be injected into routes
+const speakerService = new SpeakerService('./data/speakers.json')
+const feedbackService = new FeedbackService('./data/feedback.json')
+
+//Using routes
+app.use('/', routes({speakerService, feedbackService}))
+
+//App listening on port
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+})
