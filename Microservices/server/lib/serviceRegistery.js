69216478686de485bae0ce66b762be40bf3e7918
AhmedGhoniem01 <ahmed.ghoniem01@gmail.com>
2023-05-18 23:55:48 +0200

    Added service-registery's lib function and server route

---
+++ Microservices/server/lib/serviceRegistery.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+
+const config = require('../../config/config')[process.env.NODE_ENV || "development"]
+const log = config.log()
+const timeout = config.timeout
+
+class ServiceRegistery {
+    constructor() {
+        this.services = {}
+        this.timeout = timeout
+        this.log = log 
+    }
+
+    registerService(serviceName, serviceVersion, serviceIP, servicePort){
+        const serviceID = `${serviceName}:${serviceVersion}/${serviceIP}/${servicePort}`
+
+        if (!this.services[serviceID]){
+            const service = {
+                Name: serviceName,
+                Version: serviceVersion,
+                IP: serviceIP,
+                Port: servicePort,
+                timestamp: new Date().toLocaleString()
+            }
+            this.services[serviceID] = service
+            this.log.debug(`Added new Microservice: ${serviceID}`)
+            return serviceID
+        }
+        this.services[serviceID].serviceName = serviceName
+        this.services[serviceID].serviceVersion = serviceVersion
+        this.services[serviceID].serviceIP = serviceIP
+        this.services[serviceID].servicePort = servicePort
+        this.services[serviceID].timestamp = new Date().toLocaleString()
+        this.log.debug(`Updated Microservice: ${serviceID}`)
+
+        return serviceID
+    }
+
+}
+
+module.exports = ServiceRegistery
--- Microservices/server/server.js
+++ Microservices/server/server.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const service = express()
 
+const ServiceRegistery = require('./lib/serviceRegistery')
+
 module.exports = (config) => {
     const log = config.log()
+    const serviceRegistery = new ServiceRegistery()
 
     if(service.get('env') == "development"){
         service.use((req, res, next) => {
@@ -11,11 +14,30 @@ module.exports = (config) => {
         })
     }
 
+    service.put('/register/:serviceName/:serviceVersion/:servicePort/', (req, res) => {
+        try{
+            const {serviceName, serviceVersion, servicePort} = req.params;
+            const serviceIP = req.connection.remoteAddress.includes('::') ? `[${req.connection.remoteAddress}]` : req.connection.remoteAddress;
+            const serviceID = serviceRegistery.registerService(serviceName, serviceVersion, serviceIP, servicePort);
+            return res.status(200).json({Message: "Register successful", Result: serviceID}) 
+        }catch(err){
+            console.log(`Internal server error: ${err.message}`)
+            return res.status(err.status || 500).json({Error: err.message})
+        }   
+    })
+
+    service.get('register/:serviceName/:serviceVersion/:servicePort/', (req, res, next) => {
+        return next()
+    })
+
+    service.delete('register/:serviceName/:serviceVersion/:servicePort/', (req, res, next) => {
+        return next()
+    })
+
     service.use((error, req, res, next) => {
         log.error(`Error: ${error.message}`)
         res.status(500 || error.status).json({error: error.message})
     })
 
     return service
-    
 }
